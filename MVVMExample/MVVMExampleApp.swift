df5c1be5a21bdc12fd18aa7c35762de72199ea23
AntonLan <lanqel@gmail.com>
2023-09-15 10:06:31 +0300

    Initial Commit

---
+++ MVVMExample/MVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExampleApp.swift
+//  MVVMExample
+//
+//  Created by Anton Gerasimov on 15.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
